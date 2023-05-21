61f4ee81606455d856d848ed2117e5fc2af93fa5
lisang <ranchangxin@gmail.com>
2023-05-21 16:20:31 +0800

    移除lombok @Slf4j注解

--- src/main/java/com/zeroxn/pay/module/alipay/exception/AlipayPayException.java
+++ src/main/java/com/zeroxn/pay/module/alipay/exception/AlipayPayException.java
@@ -1,14 +1,12 @@
 package com.zeroxn.pay.module.alipay.exception;
 
 import com.zeroxn.pay.core.exception.PayException;
-import lombok.extern.slf4j.Slf4j;
 
 /**
  * @Author: lisang
  * @DateTime: 2023/4/27 16:39
  * @Description: 支付宝支付异常
  */
-@Slf4j
 public class AlipayPayException extends PayException {
     public AlipayPayException(String message) {
         super(message);
