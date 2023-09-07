95575b1e5879a9394031278902919bd60e57227e
lisang <ranchangxin@gmail.com>
2023-09-07 21:33:17 +0800

    测试PayPal模块

--- aggregate-pay-test/src/test/java/com/zeroxn/pay/test/PaypalPayTest.java
+++ aggregate-pay-test/src/test/java/com/zeroxn/pay/test/PaypalPayTest.java
@@ -1,18 +1,9 @@
 package com.zeroxn.pay.test;
 
 import com.zeroxn.pay.module.paypal.business.PaypalBusiness;
-import com.zeroxn.pay.module.paypal.config.EnablePaypalPay;
-import com.zeroxn.pay.module.paypal.config.PaypalAutoConfiguration;
-import com.zeroxn.pay.module.paypal.constant.PaypalConstant;
-import com.zeroxn.pay.module.paypal.dto.AuthResult;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
-import org.springframework.http.*;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
-import org.springframework.web.client.RestTemplate;
 
 /**
  * @Author: lisang
@@ -28,5 +19,35 @@ public class PaypalPayTest {
     @Test
     public void testGetAuthToken() {
 
+    }
+
+    @Test
+    public void testCreaterder() {
+        String result = paypalBusiness.createOrder("45315513113212", "测试订单", 10000, "https://www.baidu.com", "https://www.baidu.com");
+        System.out.println(result);
+    }
+
+    @Test
+    public void testQueryOrder() {
+        String result = paypalBusiness.queryOrder("1KH983898U558151G");
+        System.out.println(result);
+    }
+
+    @Test
+    public void testConfirmOrder() {
+        String result = paypalBusiness.confirmOrder("1KH983898U558151G");
+        System.out.println(result);
+    }
+
+    @Test
+    public void testRefundOrder() {
+        String result = paypalBusiness.refundOrder("8W915719D6330002U");
+        System.out.println(result);
+    }
+
+    @Test
+    public void testQueryRefund() {
+        String result = paypalBusiness.queryRefundOrder("06266284FF6920043");
+        System.out.println(result);
     }
 }
