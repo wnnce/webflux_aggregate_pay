e245d1f33687bae0c481b7ec246a495485890362
lisang <ranchangxin@gmail.com>
2023-09-08 16:31:34 +0800

    更改请求响应的返回数据类型

--- aggregate-pay-paypal/src/main/java/com/zeroxn/pay/module/paypal/business/PaypalBusiness.java
+++ aggregate-pay-paypal/src/main/java/com/zeroxn/pay/module/paypal/business/PaypalBusiness.java
@@ -1,17 +1,26 @@
 package com.zeroxn.pay.module.paypal.business;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.util.JSONPObject;
 import com.zeroxn.pay.core.exception.PayServiceException;
 import com.zeroxn.pay.module.paypal.config.PaypalProperties;
 import com.zeroxn.pay.module.paypal.constant.PaypalConstant;
-import com.zeroxn.pay.module.paypal.dto.AuthResult;
+import com.zeroxn.pay.module.paypal.modle.AuthResult;
+import org.apache.kafka.common.protocol.types.Field;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.*;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Map;
 import java.util.UUID;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * @Author: lisang
@@ -22,12 +31,13 @@ public class PaypalBusiness {
     private static final Logger logger = LoggerFactory.getLogger(PaypalBusiness.class);
     private final PaypalProperties properties;
     private final RestTemplate restTemplate;
+
     public PaypalBusiness(PaypalProperties properties, RestTemplate restTemplate) {
         this.properties = properties;
         this.restTemplate = restTemplate;
     }
 
-    public String createOrder(String referenceId, String description, Integer price, String returnUrl, String cancelUrl){
+    public ResponseEntity<Map<String, Object>> createOrder(String referenceId, String description, Integer price, String returnUrl, String cancelUrl){
         HttpHeaders headers = new HttpHeaders();
         headers.set("PayPal-Request-Id", UUID.randomUUID().toString());
         headers.setContentType(MediaType.APPLICATION_JSON);
@@ -57,38 +67,33 @@ public class PaypalBusiness {
                 }
                 """.formatted(referenceId, description, properties.getCurrency(), amount, returnUrl, cancelUrl).stripIndent().replaceAll("\n", "");
         HttpEntity<String> entity = new HttpEntity<>(requestBody, headers);
-        ResponseEntity<String> response = restTemplate.exchange(PaypalConstant.CREATE_ORDER_URL, HttpMethod.POST, entity, String.class);
-        return handleResponse(response);
+        return sendRequest(PaypalConstant.CREATE_ORDER_URL, HttpMethod.POST, entity);
     }
 
-    public String confirmOrder(String orderId){
+    public ResponseEntity<Map<String, Object>> confirmOrder(String orderId){
         String url = PaypalConstant.CAPTURE_ORDER_URL.replace("{id}", orderId);
         HttpHeaders headers = makeRequestHeaders();
         HttpEntity<String> entity = new HttpEntity<>(headers);
-        ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.POST, entity, String.class);
-        return handleResponse(response);
+        return sendRequest(url, HttpMethod.POST, entity);
     }
 
-    public String queryOrder(String orderId) {
+    public ResponseEntity<Map<String, Object>> queryOrder(String orderId) {
         String url = PaypalConstant.QUERY_ORDER_URL.replace("{id}", orderId);
         HttpHeaders headers = makeRequestHeaders();
         HttpEntity<String> entity = new HttpEntity<>(headers);
-        ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
-        return handleResponse(response);
+        return sendRequest(url, HttpMethod.GET, entity);
     }
 
-    public String refundOrder(String captureId) {
+    public ResponseEntity<Map<String, Object>> refundOrder(String captureId) {
         String url = PaypalConstant.REFUND_ORDER_URL.replace("{id}", captureId);
         HttpEntity<String> entity = new HttpEntity<>(makeRequestHeaders());
-        ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.POST, entity, String.class);
-        return handleResponse(response);
+        return sendRequest(url, HttpMethod.POST, entity);
     }
 
-    public String queryRefundOrder(String refundId) {
+    public ResponseEntity<Map<String, Object>> queryRefundOrder(String refundId) {
         String url = PaypalConstant.QUERY_REFUND_URL.replace("{id}", refundId);
         HttpEntity<String> entity = new HttpEntity<>(makeRequestHeaders());
-        ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
-        return handleResponse(response);
+        return sendRequest(url, HttpMethod.GET, entity);
     }
 
     public void initAuthorizationToken() {
@@ -108,16 +113,33 @@ public class PaypalBusiness {
         }
     }
 
+    private <T> ResponseEntity<Map<String, Object>> sendRequest(String url, HttpMethod method, HttpEntity<T> entity) {
+        try{
+            return restTemplate.exchange(
+                    url, method, entity,
+                    new ParameterizedTypeReference<Map<String, Object>>() {});
+        }catch (RestClientException e) {
+            logger.error("PayPal请求失败，错误消息：{}", e.getMessage());
+            throw new PayServiceException(handleErrorResponse(e.getMessage()));
+        }
+    }
+
     private HttpHeaders makeRequestHeaders() {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
         headers.setBearerAuth(properties.getToken());
         return headers;
     }
-    private String handleResponse(ResponseEntity<String> response) {
-        if (!response.getStatusCode().is2xxSuccessful()){
-            throw new PayServiceException(response.getBody());
+
+    private String handleErrorResponse(String errorMessage)  {
+        String errorCode = errorMessage.substring(0, errorMessage.indexOf(':'));
+        String regex = "\"message\":\"([^\"]*)\"";
+        Pattern compile = Pattern.compile(regex);
+        Matcher matcher = compile.matcher(errorMessage);
+        String message = "";
+        if (matcher.find()) {
+            message = matcher.group(1);
         }
-        return response.getBody();
+        return errorCode + "  " + message;
     }
 }
