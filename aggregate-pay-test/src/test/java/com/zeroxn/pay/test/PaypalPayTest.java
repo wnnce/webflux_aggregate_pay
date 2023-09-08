c452c2e2a622752a1ea9f6c73cf6f2197c773351
lisang <ranchangxin@gmail.com>
2023-09-08 16:31:58 +0800

    去除测试方法

--- aggregate-pay-test/src/test/java/com/zeroxn/pay/test/PaypalPayTest.java
+++ aggregate-pay-test/src/test/java/com/zeroxn/pay/test/PaypalPayTest.java
@@ -13,41 +13,4 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 public class PaypalPayTest {
 
-    @Autowired
-    private PaypalBusiness paypalBusiness;
-
-    @Test
-    public void testGetAuthToken() {
-
-    }
-
-    @Test
-    public void testCreaterder() {
-        String result = paypalBusiness.createOrder("45315513113212", "测试订单", 10000, "https://www.baidu.com", "https://www.baidu.com");
-        System.out.println(result);
-    }
-
-    @Test
-    public void testQueryOrder() {
-        String result = paypalBusiness.queryOrder("1KH983898U558151G");
-        System.out.println(result);
-    }
-
-    @Test
-    public void testConfirmOrder() {
-        String result = paypalBusiness.confirmOrder("1KH983898U558151G");
-        System.out.println(result);
-    }
-
-    @Test
-    public void testRefundOrder() {
-        String result = paypalBusiness.refundOrder("8W915719D6330002U");
-        System.out.println(result);
-    }
-
-    @Test
-    public void testQueryRefund() {
-        String result = paypalBusiness.queryRefundOrder("06266284FF6920043");
-        System.out.println(result);
-    }
 }
